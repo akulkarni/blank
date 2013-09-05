07ce544831474dbd94179e3db415a2f903cd6bec
acoustik <ajay@acoustik.org>
2013-09-05 16:52:13 -0400

    init

---
+++ Gemfile
@@ -0,0 +1,5 @@
+gem 'rails', '3.2.12'
+gem 'pg' 
+gem 'thin'
+gem 'haml', '>= 2.2.0'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blank::Application.config.secret_token = '6861e02628d1129eb3799c09e3853c87b848f5f4d5100477f6962fb6bea8be2c65d1d98ea30b5a4be0218ff963a4ea6094a9b8e09b7b687c88e273b3129f0ab9'
